1739a130c2992a0db7498225f0948dc67884dcaf
agentzh (章亦春) <agentzh@gmail.com>
2009-12-08 10:57:25 +0800

    checked in Test::Nginx::LWP and Test::Nginx::Socket from chunkin-nginx-module.

---
+++ lib/Test/Nginx/Socket.pm
@@ -0,0 +1,646 @@
+package Test::Nginx::Socket;
+
+use lib 'lib';
+use lib 'inc';
+use Test::Base -Base;
+use Data::Dumper;
+
+our $NoNginxManager = 0;
+our $RepeatEach = 1;
+our $Timeout = 2;
+
+use Time::HiRes qw(sleep time);
+use Test::LongString;
+
+#use Smart::Comments::JSON '###';
+use Fcntl qw(F_GETFL F_SETFL O_NONBLOCK);
+use POSIX qw(EAGAIN);
+use IO::Socket;
+
+use HTTP::Response;
+use Module::Install::Can;
+use List::Util qw( shuffle );
+use File::Spec ();
+use Cwd qw( cwd );
+
+our $Workers                = 1;
+our $WorkerConnections      = 1024;
+our $LogLevel               = 'debug';
+#our $MasterProcessEnabled   = 'on';
+#our $DaemonEnabled          = 'on';
+our $ServerPort             = 1984;
+#our $ServerPortForClient    = 1200;
+our $ServerPortForClient    = 1984;
+
+our $NginxVersion;
+our $NginxRawVersion;
+
+#our ($PrevRequest, $PrevConfig);
+
+our $ServRoot   = File::Spec->catfile(cwd(), 't/servroot');
+our $LogDir     = File::Spec->catfile($ServRoot, 'logs');
+our $ErrLogFile = File::Spec->catfile($LogDir, 'error.log');
+our $AccLogFile = File::Spec->catfile($LogDir, 'access.log');
+our $HtmlDir    = File::Spec->catfile($ServRoot, 'html');
+our $ConfDir    = File::Spec->catfile($ServRoot, 'conf');
+our $ConfFile   = File::Spec->catfile($ConfDir, 'nginx.conf');
+our $PidFile    = File::Spec->catfile($LogDir, 'nginx.pid');
+
+our @EXPORT = qw( plan run_tests run_test );
+
+=begin cmt
+
+sub plan (@) {
+    if (@_ == 2 && $_[0] eq 'tests' && defined $RepeatEach) {
+        #$_[1] *= $RepeatEach;
+    }
+    super;
+}
+
+=end cmt
+
+=cut
+
+sub send_request ($);
+
+sub trim ($);
+
+sub show_all_chars ($);
+
+sub parse_headers ($);
+
+sub run_test_helper ($$);
+
+sub get_canon_version (@) {
+    sprintf "%d.%03d%03d", $_[0], $_[1], $_[2];
+}
+
+sub get_nginx_version () {
+    my $out = `nginx -V 2>&1`;
+    if (!defined $out || $? != 0) {
+        warn "Failed to get the version of the Nginx in PATH.\n";
+    }
+    if ($out =~ m{nginx/(\d+)\.(\d+)\.(\d+)}s) {
+        $NginxRawVersion = "$1.$2.$3";
+        return get_canon_version($1, $2, $3);
+    }
+    warn "Failed to parse the output of \"nginx -V\": $out\n";
+    return undef;
+}
+
+sub run_tests () {
+    $NginxVersion = get_nginx_version();
+
+    if (defined $NginxVersion) {
+        #warn "[INFO] Using nginx version $NginxVersion ($NginxRawVersion)\n";
+    }
+
+    for my $block (shuffle blocks()) {
+        #for (1..3) {
+            run_test($block);
+        #}
+    }
+}
+
+sub setup_server_root () {
+    if (-d $ServRoot) {
+        #sleep 0.5;
+        #die ".pid file $PidFile exists.\n";
+        system("rm -rf t/servroot > /dev/null") == 0 or
+            die "Can't remove t/servroot";
+        #sleep 0.5;
+    }
+    mkdir $ServRoot or
+        die "Failed to do mkdir $ServRoot\n";
+    mkdir $LogDir or
+        die "Failed to do mkdir $LogDir\n";
+    mkdir $HtmlDir or
+        die "Failed to do mkdir $HtmlDir\n";
+    mkdir $ConfDir or
+        die "Failed to do mkdir $ConfDir\n";
+}
+
+sub write_config_file ($) {
+    my $rconfig = shift;
+    open my $out, ">$ConfFile" or
+        die "Can't open $ConfFile for writing: $!\n";
+    print $out <<_EOC_;
+worker_processes  $Workers;
+daemon on;
+master_process on;
+error_log $ErrLogFile $LogLevel;
+pid       $PidFile;
+
+http {
+    access_log $AccLogFile;
+
+    default_type text/plain;
+    keepalive_timeout  2;
+    server {
+        listen          $ServerPort;
+        server_name     localhost;
+
+        client_max_body_size 30M;
+        #client_body_buffer_size 4k;
+
+        # Begin test case config...
+$$rconfig
+        # End test case config.
+
+        location / {
+            root $HtmlDir;
+            index index.html index.htm;
+        }
+    }
+}
+
+events {
+    worker_connections  $WorkerConnections;
+}
+
+_EOC_
+    close $out;
+}
+
+sub parse_request ($$) {
+    my ($name, $rrequest) = @_;
+    open my $in, '<', $rrequest;
+    my $first = <$in>;
+    if (!$first) {
+        Test::More::BAIL_OUT("$name - Request line should be non-empty");
+        die;
+    }
+    $first =~ s/^\s+|\s+$//gs;
+    my ($meth, $rel_url) = split /\s+/, $first, 2;
+    if (!defined $rel_url) {
+        $rel_url = "/";
+    }
+    #my $url = "http://localhost:$ServerPortForClient" . $rel_url;
+
+    my $content = do { local $/; <$in> };
+    if (!defined $content) {
+        $content = "";
+    }
+    #warn Dumper($content);
+
+    close $in;
+
+    return {
+        method  => $meth,
+        url     => $rel_url,
+        content => $content,
+    };
+}
+
+sub get_pid_from_pidfile ($) {
+    my ($name) = @_;
+    open my $in, $PidFile or
+        Test::More::BAIL_OUT("$name - Failed to open the pid file $PidFile for reading: $!");
+    my $pid = do { local $/; <$in> };
+    #warn "Pid: $pid\n";
+    close $in;
+    $pid;
+}
+
+sub chunk_it ($$$) {
+    my ($chunks, $start_delay, $middle_delay) = @_;
+    my $i = 0;
+    return sub {
+        if ($i == 0) {
+            if ($start_delay) {
+                sleep($start_delay);
+            }
+        } elsif ($middle_delay) {
+            sleep($middle_delay);
+        }
+        return $chunks->[$i++];
+    }
+}
+
+sub run_test ($) {
+    my $block = shift;
+    my $name = $block->name;
+    my $request = $block->request;
+    if (!defined $request
+            && !defined $block->pipelined_requests) {
+        #$request = $PrevRequest;
+        #$PrevRequest = $request;
+        Test::More::BAIL_OUT("$name - No '--- request' section nor ---pipelined_requests specified");
+        die;
+    }
+
+    my $config = $block->config;
+    if (!defined $config) {
+        Test::More::BAIL_OUT("$name - No '--- config' section specified");
+        #$config = $PrevConfig;
+        die;
+    }
+
+    if (!$NoNginxManager) {
+        my $nginx_is_running = 1;
+        if (-f $PidFile) {
+            my $pid = get_pid_from_pidfile($name);
+            if (system("ps $pid > /dev/null") == 0) {
+                write_config_file(\$config);
+                if (kill(1, $pid) == 0) { # send HUP signal
+                    Test::More::BAIL_OUT("$name - Failed to send signal to the nginx process with PID $pid using signal HUP");
+                }
+                sleep 0.02;
+            } else {
+                unlink $PidFile or
+                    die "Failed to remove pid file $PidFile\n";
+                undef $nginx_is_running;
+            }
+        } else {
+            undef $nginx_is_running;
+        }
+
+        unless ($nginx_is_running) {
+            warn "*** Restarting the nginx server...\n";
+            setup_server_root();
+            write_config_file(\$config);
+            if ( ! Module::Install::Can->can_run('nginx') ) {
+                Test::More::BAIL_OUT("$name - Cannot find the nginx executable in the PATH environment");
+                die;
+            }
+        #if (system("nginx -p $ServRoot -c $ConfFile -t") != 0) {
+        #Test::More::BAIL_OUT("$name - Invalid config file");
+        #}
+        #my $cmd = "nginx -p $ServRoot -c $ConfFile > /dev/null";
+
+            my $cmd;
+            if ($NginxVersion >= 0.007053) {
+                $cmd = "nginx -p $ServRoot/ -c $ConfFile > /dev/null";
+            } else {
+                $cmd = "nginx -c $ConfFile > /dev/null";
+            }
+
+            if (system($cmd) != 0) {
+                Test::More::BAIL_OUT("$name - Cannot start nginx using command \"$cmd\".");
+                die;
+            }
+            sleep 0.1;
+        }
+    }
+
+    my $skip_nginx = $block->skip_nginx;
+    my ($tests_to_skip, $should_skip, $skip_reason);
+    if (defined $skip_nginx) {
+        if ($skip_nginx =~ m{
+                ^ \s* (\d+) \s* : \s*
+                    ([<>]=?) \s* (\d+)\.(\d+)\.(\d+)
+                    (?: \s* : \s* (.*) )?
+                \s*$}x) {
+            $tests_to_skip = $1;
+            my ($op, $ver1, $ver2, $ver3) = ($2, $3, $4, $5);
+            $skip_reason = $6;
+            my $ver = get_canon_version($ver1, $ver2, $ver3);
+            if ((!defined $NginxVersion and $op =~ /^</)
+                    or eval "$NginxVersion $op $ver")
+            {
+                $should_skip = 1;
+            }
+        } else {
+            Test::More::BAIL_OUT("$name - Invalid --- skip_nginx spec: " .
+                $skip_nginx);
+            die;
+        }
+    }
+    if (!defined $skip_reason) {
+        $skip_reason = "various reasons";
+    }
+
+    my $todo_nginx = $block->todo_nginx;
+    my ($should_todo, $todo_reason);
+    if (defined $todo_nginx) {
+        if ($todo_nginx =~ m{
+                ^ \s*
+                    ([<>]=?) \s* (\d+)\.(\d+)\.(\d+)
+                    (?: \s* : \s* (.*) )?
+                \s*$}x) {
+            my ($op, $ver1, $ver2, $ver3) = ($1, $2, $3, $4);
+            $todo_reason = $5;
+            my $ver = get_canon_version($ver1, $ver2, $ver3);
+            if ((!defined $NginxVersion and $op =~ /^</)
+                    or eval "$NginxVersion $op $ver")
+            {
+                $should_todo = 1;
+            }
+        } else {
+            Test::More::BAIL_OUT("$name - Invalid --- todo_nginx spec: " .
+                $todo_nginx);
+            die;
+        }
+    }
+    if (!defined $todo_reason) {
+        $todo_reason = "various reasons";
+    }
+
+    my $i = 0;
+    while ($i++ < $RepeatEach) {
+        if ($should_skip) {
+            SKIP: {
+                skip "$name - $skip_reason", $tests_to_skip;
+
+                run_test_helper($block, $request);
+            }
+        } elsif ($should_todo) {
+            TODO: {
+                local $TODO = "$name - $todo_reason";
+
+                run_test_helper($block, $request);
+            }
+        } else {
+            run_test_helper($block, $request);
+        }
+    }
+}
+
+sub trim ($) {
+    (my $s = shift) =~ s/^\s+|\s+$//g;
+    $s =~ s/\n/ /gs;
+    $s =~ s/\s{2,}/ /gs;
+    $s;
+}
+
+sub show_all_chars ($) {
+    my $s = shift;
+    $s =~ s/\n/\\n/gs;
+    $s =~ s/\r/\\r/gs;
+    $s =~ s/\t/\\t/gs;
+    $s;
+}
+
+sub parse_headers ($) {
+    my $s = shift;
+    my %headers;
+    open my $in, '<', \$s;
+    while (<$in>) {
+        s/^\s+|\s+$//g;
+        my ($key, $val) = split /\s*:\s*/, $_, 2;
+        $headers{$key} = $val;
+    }
+    close $in;
+    return \%headers;
+}
+
+sub run_test_helper ($$) {
+    my ($block, $request) = @_;
+
+    my $name = $block->name;
+
+    my $is_chunked = 0;
+    my $more_headers = '';
+    if ($block->more_headers) {
+        my @headers = split /\n+/, $block->more_headers;
+        for my $header (@headers) {
+            next if $header =~ /^\s*\#/;
+            my ($key, $val) = split /:\s*/, $header, 2;
+            if (lc($key) eq 'transfer-encoding' and $val eq 'chunked') {
+                $is_chunked = 1;
+            }
+            #warn "[$key, $val]\n";
+            $more_headers .= "$key: $val\r\n";
+        }
+    }
+
+    my $req;
+    if ($block->pipelined_requests) {
+        my $reqs = $block->pipelined_requests;
+        if (!ref $reqs || ref $reqs ne 'ARRAY') {
+            Test::More::BAIL_OUT("$name - invalid entries in --- pipelined_requests");
+        }
+        my $i = 0;
+        for my $request (@$reqs) {
+            my $conn_type;
+            if ($i++ == @$reqs - 1) {
+                $conn_type = 'close';
+            } else {
+                $conn_type = 'keep-alive';
+            }
+            my $parsed_req = parse_request($name, \$request);
+
+            my $len_header = '';
+            if (!$is_chunked && defined $parsed_req->{content} && $parsed_req->{content} ne '') {
+                $parsed_req->{content} =~ s/^\s+|\s+$//gs;
+
+                $len_header .= "Content-Length: " . length($parsed_req->{content}) . "\r\n";
+            }
+
+            $req .= "$parsed_req->{method} $parsed_req->{url} HTTP/1.1\r
+Host: localhost\r
+Connection: $conn_type\r
+$more_headers$len_header\r
+$parsed_req->{content}";
+        }
+    } else {
+        my $parsed_req = parse_request($name, \$request);
+        ### $parsed_req
+
+        my $len_header = '';
+        if (!$is_chunked && defined $parsed_req->{content} && $parsed_req->{content} ne '') {
+            $parsed_req->{content} =~ s/^\s+|\s+$//gs;
+            $len_header .= "Content-Length: " . length($parsed_req->{content}) . "\r\n";
+        }
+
+        $req = "$parsed_req->{method} $parsed_req->{url} HTTP/1.1\r
+Host: localhost\r
+Connection: Close\r
+$more_headers$len_header\r
+$parsed_req->{content}";
+    }
+
+    if (!$req) {
+        Test::More::BAIL_OUT("$name - request empty");
+    }
+
+    #warn "request: $req\n";
+
+    my $raw_resp = send_request($req);
+
+    #warn "raw resonse: [$raw_resp]\n";
+
+    my $res = HTTP::Response->parse($raw_resp);
+    my $enc = $res->header('Transfer-Encoding');
+
+    if (defined $enc && $enc eq 'chunked') {
+        #warn "Found chunked!";
+        my $raw = $res->content;
+        if (!defined $raw) {
+            $raw = '';
+        }
+
+        my $decoded = '';
+        while (1) {
+            if ($raw =~ /\G0\r\n\r\n$/gcs) {
+                last;
+            }
+            if ($raw =~ m{ \G \ * ( [A-Fa-f0-9]+ ) \ * \r\n }gcsx) {
+                my $rest = hex($1);
+                #warn "chunk size: $rest\n";
+                if ($raw =~ /\G(.{$rest})\r\n/gcs) {
+                    $decoded .= $1;
+                    #warn "decoded: [$1]\n";
+                } else {
+                    fail("$name - invalid chunked data received.");
+                    return;
+                }
+            } elsif ($raw =~ /\G.+/gcs) {
+                fail "$name - invalid chunked body received: $&";
+                return;
+            } else {
+                fail "$name - no last chunk found";
+                return;
+            }
+        }
+        #warn "decoded: $decoded\n";
+        $res->content($decoded);
+    }
+
+    if (defined $block->error_code) {
+        is($res->code, $block->error_code, "$name - status code ok");
+    } else {
+        is($res->code, 200, "$name - status code ok");
+    }
+
+    if (defined $block->response_headers) {
+        my $headers = parse_headers($block->response_headers);
+        while (my ($key, $val) = each %$headers) {
+            my $expected_val = $res->header($key);
+            if (!defined $expected_val) {
+                $expected_val = '';
+            }
+            is $expected_val, $val,
+                "$name - header $key ok";
+        }
+    } elsif (defined $block->response_headers_like) {
+        my $headers = parse_headers($block->response_headers_like);
+        while (my ($key, $val) = each %$headers) {
+            my $expected_val = $res->header($key);
+            if (!defined $expected_val) {
+                $expected_val = '';
+            }
+            like $expected_val, qr/^$val$/,
+                "$name - header $key like ok";
+        }
+    }
+
+    if (defined $block->response_body) {
+        my $content = $res->content;
+        if (defined $content) {
+            $content =~ s/^TE: deflate,gzip;q=0\.3\r\n//gms;
+            $content =~ s/^Connection: TE, close\r\n//gms;
+        }
+
+        my $expected = $block->response_body;
+        $expected =~ s/\$ServerPort\b/$ServerPort/g;
+        $expected =~ s/\$ServerPortForClient\b/$ServerPortForClient/g;
+        #warn show_all_chars($content);
+
+        is_string($content, $expected, "$name - response_body - response is expected");
+        #is($content, $expected, "$name - response_body - response is expected");
+
+    } elsif (defined $block->response_body_like) {
+        my $content = $res->content;
+        if (defined $content) {
+            $content =~ s/^TE: deflate,gzip;q=0\.3\r\n//gms;
+        }
+        $content =~ s/^Connection: TE, close\r\n//gms;
+        my $expected_pat = $block->response_body_like;
+        $expected_pat =~ s/\$ServerPort\b/$ServerPort/g;
+        $expected_pat =~ s/\$ServerPortForClient\b/$ServerPortForClient/g;
+        my $summary = trim($content);
+        like($content, qr/$expected_pat/s, "$name - response_body_like - response is expected ($summary)");
+    }
+}
+
+sub send_request ($) {
+    my $write_buf = shift;
+
+    my $sock = IO::Socket::INET->new(
+        PeerAddr => 'localhost',
+        PeerPort => $ServerPortForClient,
+        Proto    => 'tcp'
+    ) or die "Can't connect to localhost:$ServerPortForClient: $!\n";
+
+    my $flags = fcntl $sock, F_GETFL, 0
+        or die "Failed to get flags: $!\n";
+
+    fcntl $sock, F_SETFL, $flags | O_NONBLOCK
+        or die "Failed to set flags: $!\n";
+
+    my $resp = '';
+    my $write_offset = 0;
+    my $buf_size = 1024;
+
+    my $now = time;
+    while (1) {
+        if (time - $now >= $Timeout) {
+            warn "timed out\n";
+            return $resp;
+        }
+        #warn "main loop...";
+        my $read_buf;
+        my $bytes = sysread($sock, $read_buf, $buf_size);
+
+        if (!defined $bytes) {
+            if ($! == EAGAIN) {
+                #warn "read again...";
+                goto write_sock;
+            }
+            return "500 read failed: $!";
+        }
+        if ($bytes == 0) {
+            close $sock;
+            #warn "returning response: $resp\n";
+            return $resp;
+        }
+        $resp .= $read_buf;
+        #warn "read $bytes ($read_buf) bytes.\n";
+
+write_sock:
+        my $rest = length($write_buf) - $write_offset;
+        #warn "offset: $write_offset, rest: $rest, length ", length($write_buf), "\n";
+        #die;
+
+        if ($rest > 0) {
+            $bytes = syswrite($sock, $write_buf, $rest, $write_offset);
+
+            if (!defined $bytes) {
+                if ($! == EAGAIN) {
+                    #warn "write again...";
+                    next;
+                }
+                my $errmsg = "write failed: $!";
+                warn "$errmsg\n";
+                if (!$resp) {
+                    return "$errmsg";
+                }
+                return $resp;
+            }
+
+            #warn "wrote $bytes bytes.\n";
+            $write_offset += $bytes;
+        }
+    }
+    return $resp;
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Test::Nginx::Socket - Test scaffold for the Nginx C modules
+
+=head1 AUTHOR
+
+agentzh C<< <agentzh@gmail.com> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright (C) 2009 by agentzh.
+Copyright (C) 2009 by Taobao Inc. ( http://www.taobao.com )
+
+This software is licensed under the terms of the BSD License.
+
