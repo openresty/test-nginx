273517cb12e365b7802497837f217f5990953601
Yichun Zhang (agentzh) <agentzh@gmail.com>
2015-10-15 16:12:31 +0800

    Test::Nginx::Socket::Lua: added support for init_by_lua_block {}.

--- lib/Test/Nginx/Socket/Lua.pm
+++ lib/Test/Nginx/Socket/Lua.pm
@@ -5,16 +5,18 @@ use Test::Nginx::Socket -Base;
 my $code = $ENV{TEST_NGINX_INIT_BY_LUA};
 
 if ($code) {
-    $code =~ s/\\/\\\\/g;
-    $code =~ s/['"]/\\$&/g;
+    (my $escaped_code = $code) =~ s/\\/\\\\/g;
+    $escaped_code =~ s/['"]/\\$&/g;
 
     Test::Nginx::Socket::set_http_config_filter(sub {
         my $config = shift;
         if ($config =~ /init_by_lua_file/) {
             return $config;
         }
-        unless ($config =~ s{(?<!\#  )(?<!\# )(?<!\#)init_by_lua\s*(['"])((?:\\.|.)*)\1\s*;}{init_by_lua $1$code$2$1;}s) {
-            $config .= "init_by_lua '$code';";
+        unless ($config =~ s{(?<!\#  )(?<!\# )(?<!\#)init_by_lua\s*(['"])((?:\\.|.)*)\1\s*;}{init_by_lua $1$escaped_code$2$1;}s) {
+            unless ($config =~ s{(?<!\#  )(?<!\# )(?<!\#)init_by_lua_block\s*\{}{init_by_lua_block \{ $code }s) {
+                $config .= "init_by_lua '$escaped_code';";
+            }
         }
         return $config;
     });
